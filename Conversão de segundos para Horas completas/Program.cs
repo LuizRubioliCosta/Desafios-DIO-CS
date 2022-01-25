af4a084101397e78b4c756d95aef5a2f6c66a6e7
Luiz Rubioli <luiz.rubioli@hotmail.com>
2022-01-25 11:58:35 -0300

    Desafios Dio em C#

---
+++ Conversão de segundos para Horas completas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+    class MinhaClasse
+    {
+        static void Main(string[] args)
+        {
+            
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+       
+            var hours=0;
+            var minutes=0;
+            var seconds=0;
+            hours = timeInSeconds/3600;
+            minutes = (timeInSeconds % 3600) / 60;
+            seconds =(timeInSeconds % 3600) % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
